4a2e3f93aafe9d94bd0dba3f3c2fe95826042a13
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-26 12:32:40 +0500

    Add logger file

---
+++ config/logger.js
@@ -0,0 +1,76 @@
+var winston = require('winston');
+winston.emitErrs = true;
+var config = require('./config.js');
+require('winston-pushbullet').Pushbullet;
+require('winston-mongodb').MongoDB;
+
+var logger = new winston.Logger({
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3,
+    trace: 4,
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'blue',
+    debug: 'green',
+    trace: 'white',
+  },
+  transports: [
+    new winston.transports.MongoDB({
+
+      level: 'info',
+      silent: false,
+      db: config.db.host,
+      options: {
+        db: {
+          native_parser: true
+        },
+        server: {
+          poolSize: 2,
+          socketOptions: {
+            autoReconnect: true
+          }
+        }
+      },
+      collection: 'logs',
+      storeHost: false,
+      username: config.db.user,
+      password: config.db.pass,
+      label: "",
+      name: "",
+      capped: true,
+      cappedSize: 10000000 //10MB
+
+    }),
+    new winston.transports.Pushbullet({
+
+      apikey: config.pushbullet.apiKey,
+      level: 'error',
+      title: 'Payload Error',
+      devices: '' // notify all devices
+
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    })
+
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
+module.exports.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  }
+};
+
+
+module.exports = logger;
